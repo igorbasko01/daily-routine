f0a92a9ca6a33951f2bfec338abc0aba1517c1c4
igorbasko01 <igorbasko@gmail.com>
2023-12-10 07:08:26 +0200

    allow the Routine model to be JSON serialized

--- lib/models/routine.dart
+++ lib/models/routine.dart
@@ -21,6 +21,23 @@ class Routine {
         id: id, name: name, time: time, isCompleted: !isCompleted);
   }
 
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'time': time.toIso8601String(),
+      'isCompleted': isCompleted
+    };
+  }
+
+  factory Routine.fromJson(Map<String, dynamic> json) {
+    return Routine(
+        id: json['id'],
+        name: json['name'],
+        time: DateTime.parse(json['time']),
+        isCompleted: json['isCompleted']);
+  }
+
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
--- test/models/routine_test.dart
+++ test/models/routine_test.dart
@@ -14,4 +14,33 @@ void main() {
     });
   });
 
+  group('Routine Model Json Serialization', () {
+    test('Routine is correctly serialized to json', () {
+      final dateTime = DateTime(2023, 11, 30, 8, 0);
+      final routine = Routine(id: 1, name: 'Morning Exercise', time: dateTime);
+
+      expect(routine.toJson(), {
+        'id': 1,
+        'name': 'Morning Exercise',
+        'time': dateTime.toIso8601String(),
+        'isCompleted': false
+      });
+    });
+
+    test('Routine is correctly deserialized from json', () {
+      final dateTime = DateTime(2023, 11, 30, 8, 0);
+      final routine = Routine.fromJson({
+        'id': 1,
+        'name': 'Morning Exercise',
+        'time': dateTime.toIso8601String(),
+        'isCompleted': false
+      });
+
+      expect(routine.id, 1);
+      expect(routine.name, 'Morning Exercise');
+      expect(routine.time, dateTime);
+      expect(routine.isCompleted, false);
+    });
+  });
+
 }
