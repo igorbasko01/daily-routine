e268875175770818ca585f489a13b5a53e0404ad
igorbasko01 <igorbasko@gmail.com>
2023-12-14 09:50:08 +0200

    small refactor of the handle day change

--- lib/blocs/routine_bloc.dart
+++ lib/blocs/routine_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:daily_routine/blocs/routine_event.dart';
 import 'package:daily_routine/blocs/routine_state.dart';
+import 'package:daily_routine/models/routine.dart';
 import 'package:daily_routine/repositories/routine_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -51,22 +52,30 @@ class RoutineBloc extends Bloc<RoutineEvent, RoutineState> {
 
   FutureOr<void> _handleDayChange(HandleDayChangeRoutineEvent event, Emitter<RoutineState> emit) async {
     var routines = await _routineRepository.getAllRoutines();
-    if (routines.isNotEmpty) {
-      var firstRoutine = routines.first;
-      var now = DateTime.now();
-      // using the time of the first routine, because can compare only the full date time and not just the date.
-      // We want to make the time identical between the next day and the first routine. Because
-      // we don't want to reset the routines if the time is after the first routine time. Only the date is what
-      // matters.
-      var nextDayTime = DateTime(now.year, now.month, now.day, firstRoutine.time.hour, firstRoutine.time.minute);
-      if (nextDayTime.isAfter(firstRoutine.time)) {
-        var resetRoutines = await _routineRepository.resetAllRoutines();
-        emit(LoadedAllRoutineState(resetRoutines));
-      } else {
-        emit(LoadedAllRoutineState(routines));
-      }
-    } else {
+    if (routines.isEmpty) {
       emit(LoadedAllRoutineState(routines));
+      return;
+    }
+
+    var isReset = await _handleResetRoutineOnDayChange(routines.first, emit);
+    if (!isReset) {
+      emit(LoadedAllRoutineState(routines));
+    }
+  }
+
+  Future<bool> _handleResetRoutineOnDayChange(Routine routine, Emitter<RoutineState> emit) async {
+    var now = DateTime.now();
+    // using the time of the first routine, because can compare only the full date time and not just the date.
+    // We want to make the time identical between the next day and the first routine. Because
+    // we don't want to reset the routines if the time is after the first routine time. Only the date is what
+    // matters.
+    var nextDayTime = DateTime(now.year, now.month, now.day, routine.time.hour, routine.time.minute);
+    if (nextDayTime.isAfter(routine.time)) {
+      var resetRoutines = await _routineRepository.resetAllRoutines();
+      emit(LoadedAllRoutineState(resetRoutines));
+      return true;
+    } else {
+      return false;
     }
   }
 }
